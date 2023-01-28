aa2167cf226f7174a1efd2f7fe5508372fe436be
Sergey <sergey.stupnikov.59@yandex.ru>
2023-01-29 00:42:09 +0300

    first commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Сергей on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
